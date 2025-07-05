f3fa99ce56028534e8e087d6b8d01218aa5eda33
ansgrb <ansgrb@gmail.com>
2025-07-05 22:59:28 +0300

    feat: add IconSync plugin configuration and implement image resizing for iOS icons

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,16 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
 	`kotlin-dsl`
 	`maven-publish`
 	signing
-	kotlin("jvm") version "2.1.21"
+//	kotlin("jvm") version "2.1.21"
+	id("com.vanniktech.maven.publish") version "0.33.0"
 }
+
+val pluginGroupId = "com.github.ansgrb"
+val pluginArtifactId = "iconsync"
+val pluginVersion = "1.0.0"
 
 group = "com.github.ansgrb"
 version = "1.0-SNAPSHOT"
@@ -30,14 +37,47 @@ gradlePlugin {
 		create("iconsync") {
 			id = "com.github.ansgrb.iconsync"
 			implementationClass = "com.github.ansgrb.iconsync.IconSyncPlugin"
+			displayName = "KMP iOS IconSync Plugin"
+			description =
+				"A Gradle plugin to automatically sync Android launcher icons to the iOS part of a Kotlin Multiplatform project."
+
 		}
 	}
 }
 
-publishing {
-	repositories {
-		// This tells Gradle to publish to the local Maven repository
-		// typically located at ~/.m2/repository
-		mavenLocal()
+mavenPublishing {
+	coordinates(
+		pluginGroupId,
+		pluginArtifactId,
+		pluginVersion
+	)
+
+	pom {
+		name.set("KMP iOS IconSync Plugin")
+		description.set("A Gradle plugin to automatically sync Android launcher icons to the iOS part of a Kotlin Multiplatform project.")
+		inceptionYear.set("2025")
+		url.set("https://github.com/ansgrb/iconsync")
+		licenses {
+			license {
+				name.set("The Apache License, Version 2.0")
+				url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+				distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+			}
+		}
+		developers {
+			developer {
+				id.set("ansgrb")
+				name.set("Anas Ghareib")
+				url.set("https://github.com/ansgrb/")
+			}
+		}
+		scm {
+			url.set("https://github.com/ansgrb/iconsync")
+			connection.set("scm:git:git://github.com/ansgrb/iconsync.git")
+			developerConnection.set("scm:git:ssh://git@github.com/ansgrb/iconsync.git")
+		}
 	}
+
+	publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+	signAllPublications()
 }
